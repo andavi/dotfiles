595956d4b7968a6c6cd703a6c947056f703e9d57
Andrew Davis <andavi@users.noreply.github.com>
2021-07-11 01:10:29 -0700

    removes

--- macos/.local/scripts/desimp.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# script that dencrypts a file with a password
-# uses aes 256 via openssl
-
-F=$1
-openssl enc -in $1 -d -aes-256-cbc -pass stdin > ${F:0:${#F}-4}
-
--- macos/.local/scripts/ensimp.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# script that encrypts a file with a password
-# uses aes 256 via openssl
-
-openssl enc -in $1 -aes-256-cbc -pass stdin > $1.enc
-
